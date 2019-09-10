c12116f5fe06e3c02b7614d4fd1c8512bec48cf2
Park <jaguarcanine@gmail.com>
2019-09-10 12:03:56 +0900

    express

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const http = require('http');
+//const http = require('http');
+//
+//const hostname = 'ec2-3-112-2-137.ap-northeast-1.compute.amazonaws.com';
+//const port = 3000;
+//
+//const server = http.createServer((req, res) => {
+//  res.statusCode = 200;
+//  res.setHeader('Content-Type', 'text/plain');
+//  res.end('Hello World\n');
+//});
+//
+//server.listen(port, hostname, () => {
+//  console.log(`Server running at http://${hostname}:${port}/`);
+//});
+//
 
-const hostname = 'ec2-3-112-2-137.ap-northeast-1.compute.amazonaws.com';
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.get('/', (req, res) => res.send('Hello World!'))
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
